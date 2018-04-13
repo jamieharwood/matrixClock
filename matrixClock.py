ac3d690b4b7fc9768f0af4a1d0d2ac8624f6c21d
Jamie <jamieharwood007@gmail.com>
2018-04-14 00:22:47 +0100

    Clock, graph and Neo Pixel!

--- matrixClock.py
+++ matrixClock.py
@@ -4,12 +4,43 @@ import time
 import socket
 import machine
 import ustruct
+import urequests
+import ujson
+import neopixel
 
 
 addBST = 3600
 NTP_DELTA = 3155673600 - addBST
 
 host = '0.uk.pool.ntp.org'
+
+# Neo pixel simple levels
+numSensors = 3
+
+neoLow = 0
+neoMid = 64
+neoHi = 255
+
+
+def getGraphData():
+    url = "http://192.168.86.240:5000/getWeatherGraph/"
+    returnString = ''
+
+    # print(url)
+
+    try:
+        response = urequests.get(url)
+        returnString = response.text
+        response.close()
+
+        returnString = returnString.replace('\"', '')
+
+        # print(returnString)
+    except:
+        print('Fail www connect...')
+
+    return returnString
+
 
 def getntptime():
     NTP_QUERY = bytearray(48)
@@ -23,6 +54,7 @@ def getntptime():
     val = ustruct.unpack("!I", msg[40:44])[0]
     return val - NTP_DELTA
 
+
 def settime():
     t = getntptime()
     tm = time.localtime(t)
@@ -30,7 +62,18 @@ def settime():
     rtc = machine.RTC()
     rtc.datetime(tm)
 
+
 def main():
+    np = neopixel.NeoPixel(machine.Pin(4), 4)
+
+    np[3] = (neoMid, neoLow, neoLow)
+
+    np[0] = (neoMid, neoLow, neoMid)
+    np[1] = (neoMid, neoLow, neoMid)
+    np[2] = (neoMid, neoLow, neoMid)
+
+    np.write()
+
     # Set the RTC
     settime()
     rtc = RTC()
@@ -68,7 +111,7 @@ def main():
             displayTimeLast = displayTimeNow
 
         display.fill(0)
-        display.text(str(displayTimeNow),0,0,1)
+        display.text(str(displayTimeNow), 0, 0, 1)
 
 
         # *********************
@@ -76,15 +119,44 @@ def main():
 
         seconds = timeNow[6]
         if (seconds/2) == round(seconds/2):
-            #display.rect(15, 0, 2, 1, 1)
             display.rect(15, 5, 2, 2, 1)
-            #display.pixel(15, 2, 1)
-            #display.pixel(15, 5, 1)
         else:
-            #display.rect(15, 0, 2, 1, 0)
             display.rect(15, 5, 2, 2, 0)
-            #display.pixel(15, 2, 0)
-            #display.pixel(15, 5, 0)
+
+
+        # Display the graph
+        gData = getGraphData()
+
+        jData = ujson.loads(gData)
+        min = 100
+        max = 0
+
+        gOff = 8 * 8
+
+        display.line(gOff, 0, gOff, 7, 1)
+        display.line(gOff, 7, gOff + 32, 7, 1)
+
+        # Get min and max
+        sample = 0
+        for column in jData:
+            # print(jData[sample])
+
+            if column < min:
+                min = column
+            if column > max:
+                max = column
+
+            sample += 1
+
+        # Draw the bar graph
+        sample = 31
+        for column in jData:
+
+            bar = round(7 - (7 / (max - min)) * (column - min))
+
+            display.line(gOff + sample, 7, gOff + sample, bar, 1)
+
+            sample -= 1
 
         display.show()
 
